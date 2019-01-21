d8248220731129cbded04f062983ea37fab25b34
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-21 14:56:04 +0800

    取得task資料

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Task;
 use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
     //
-    public function __construct()
+    protected $tasks;
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+
+        $this->tasks = $tasks;
     }
     public function index(Request $request)
     {
-        return view('tasks.index');
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
     }
     public function store(Request $request)
     {
